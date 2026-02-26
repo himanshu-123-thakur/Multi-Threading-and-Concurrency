136ac506bb45e46d3832fd9f0906f947058d9a1d
Himanshu Thakur <himanthakur123@gmail.com>
2026-02-26 10:46:12 +0530

    DATA RACE MUTEC lock_guard

--- DataRACE_mutex.cpp
+++ DataRACE_mutex.cpp
@@ -1,15 +1,53 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
 using namespace std;
+
+//THIS CAUSES unstable shit as same resource for two thread 
+
+// void umm_thread(){
+//     for(int i=0;i>-100;i--){
+//         cout<<"From t1: "<<i<<endl;
+//     }
+// }
+// int main(){
+//     thread t (umm_thread);
+//     for(int i =1;i<100;i++){
+//         cout<<"FROM MAIN : "<<i<<endl;
+//     }
+//     t.join();
+//     return 0;
+// }
+
+//MUTEXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ---> synchronise the access of the common resource --> inn this case cout
+
+mutex mu;
+void shared_print(string msg,int id){
+
+    //mu.lock(); 
+
+
+    ///LOCKING mutex wjhile printing --> other thread wont be able to print messaage
+    //if exception then mutex is locked forever that is very very bad 
+    //TO HANDLE THAT we dont directly use mutex lock
+
+    lock_guard<mutex> guardd(mu);//HANDLE excpetion and forever locked USES RAII
+    cout<< msg << id <<endl;
+    //mu.unlock();
+
+}
+
 void umm_thread(){
     for(int i=0;i>-100;i--){
-        cout<<"From t1: "<<i<<endl;
+        // cout<<"From t1: "<<i<<endl;
+        shared_print(string("FROM t1 : "),i);
     }
 }
 int main(){
     thread t (umm_thread);
-    for(int i =-;i<100;i++){
-        cout<<"FROM MAIN : "<<i<<endl;
+    for(int i =0;i<100;i++){
+        // cout<<"FROM MAIN : "<<i<<endl;
+        shared_print(string("From MAIN : "),i);
     }
     t.join();
     return 0;
